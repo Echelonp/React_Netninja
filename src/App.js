5070b9b3ebd44038431ca3116861f3f0f225f1c6
Paradorn Pimporn <paraz@Paradorns-MacBook-Pro.local>
2020-04-12 16:17:32 +0700

    #17:Form revisited

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App extends Component {
   state = {
     name: "Ryu",
     age: 30,
+    belt: null,
   };
 
   handelClick = (e) => {
@@ -13,33 +14,33 @@ class App extends Component {
       name: "Paradorn",
     });
   };
-
   handelMouseOver = (e) => {
     console.log(e.pageX);
   };
-
   handleSubmit = (e) => {
     e.preventDefault();
     console.log("Form submited");
+    console.log(this.state);
   };
   handleChange = (e) => {
     this.setState({
-      name: e.target.value,
+      [e.target.id]: e.target.value,
     });
   };
 
   render() {
     return (
       <div className="app-content">
-        <h1>Hello, Para</h1>
-        <p>My name is {this.state.name}</p>
-        <button className="button" onClick={this.handelClick}>
-          Click Me
-        </button>
-        <button onMouseOver={this.handelMouseOver}>Hover, Me!</button>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <label htmlFor="name">Name: </label>
+          <input type="text" id="name" onChange={this.handleChange}></input>
 
-        <form className="form" onClick={this.handleSubmit}>
-          <input type="text" onChange={this.handleChange}></input>
+          <label htmlFor="name">Age: </label>
+          <input type="number" id="age" onChange={this.handleChange}></input>
+
+          <label htmlFor="name">Belt: </label>
+          <input type="text" id="belt" onChange={this.handleChange}></input>
+
           <button>Submit</button>
         </form>
       </div>
